3ee9d6f1fbb8b19d90285d770bff4f8263dba3d2
kishanrank <kishanrank763@gmail.com>
2020-06-17 22:41:46 +0530

    seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,7 +11,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(UserSeeder::class);
         $this->call(UsersTableSeeder::class);
         $this->call(SettingsTableSeeder::class);
         $this->call(CategorySeeder::class);

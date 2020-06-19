9537441d56c83ed4b2f8d4eb091eb396eeeb9abd
kishanrank <kishanrank763@gmail.com>
2020-06-19 22:36:54 +0530

    urlschema

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,7 +26,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        
         View::composer(['*'], HeaderComposer::class);
         View::composer(['*'], SideWidgetComposer::class);
     }
